919410fa4a8c9b90dcb3dbb4374b86d998304913
kawsar-uddin <kawsaruddin450@gmail.com>
2024-06-20 22:39:06 +0600

    implement modal for displaying phone features

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ const displayPhones = phones => {
             <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions justify-center">
-                <button class="btn btn-primary">Buy Now</button>
+                <button class="btn btn-primary" onclick="handleShowDetail('${phone.slug}')">Show More</button>
             </div>
         </div>
         `
@@ -32,6 +32,25 @@ const toggleLoadingDots = (isLoading) => {
     else{
         loadingDots.classList.add('hidden');
     }
+}
+const handleShowDetail = async (id) => {
+    console.log('Clicked on', id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    showDataModal(data.data)
+}
+
+const showDataModal = (phone) => {
+    show_details_modal.showModal();
+    document.getElementById('phone-name').innerText = phone.name;
+    document.getElementById('phone-thumb').setAttribute('src', phone.image);
+    document.getElementById('phone-features').innerHTML = `
+    <h5 class="font-bold text-lg">Main Features: </h5>
+    <p class="mt-1"><span class="font-semibold">Storage: </span>${phone.mainFeatures.storage}</p>
+    <p class="mt-1"><span class="font-semibold">Display: </span>${phone.mainFeatures.displaySize}</p>
+    <p class="mt-1"><span class="font-semibold">Chipset: </span>${phone.mainFeatures.chipset}</p>
+    <p class="mt-1"><span class="font-semibold">Memory: </span>${phone.mainFeatures.memory}</p>
+    `
 }
 
 document.getElementById('selection').addEventListener('change', function () {
